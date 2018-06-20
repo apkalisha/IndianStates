ba16413a79538fe0eae0aba030d1745ef2c6a816
Prakash Kumar <apkalisha@gmail.com>
2018-06-21 00:45:24 +0530

    Authentication Added

--- app/src/main/java/com/indian/states/capitals/indianstates/RegisterActivity.java
+++ app/src/main/java/com/indian/states/capitals/indianstates/RegisterActivity.java
@@ -36,10 +36,6 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
