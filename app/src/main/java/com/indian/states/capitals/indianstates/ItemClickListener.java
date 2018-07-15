32bbbbcb04b4d70683158abfe902408e61d09234
apkalisha <39853483+apkalisha@users.noreply.github.com>
2018-07-16 00:56:52 +0530

    added quiz categories

--- app/src/main/java/com/indian/states/capitals/indianstates/ItemClickListener.java
+++ app/src/main/java/com/indian/states/capitals/indianstates/ItemClickListener.java
@@ -3,6 +3,7 @@ package com.indian.states.capitals.indianstates;
 import android.view.View;
 
 public interface ItemClickListener {
+    //Interface
 
     void onClick(View view,int position,boolean isLongClick);
 }
